a5e84ce3cf8009b68c1f501fb2b8ffd75c8d181f
Juno Lee <lejuno80@gmail.com>
2018-02-27 19:46:42 -0600

    challenge 1 update

--- arrys.js
+++ arrys.js
@@ -1,1 +1,27 @@
-console.log("Arrays");
+
+
+//challenge 1
+
+var test1 = "cat";
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+var result = reverseString(test1);
+
+console.log(result);
+
+if (test1 === result){
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+// another take
+
+var test = "racecar";
+var opp = test.split("").reverse().join("");
+if (test === opp) {
+    console.log("same", true);
+}else {
+    console.log("same", false);
+}
